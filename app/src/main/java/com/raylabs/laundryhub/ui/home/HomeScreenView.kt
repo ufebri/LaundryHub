7820569f00a83dfaf33a2f852f6bd55e005572da
Uray Febri Isviladinata <ray@Urays-MacBook-Air.local>
2025-06-08 09:59:20 +0700

    refactor home

--- app/src/main/java/com/raylabs/laundryhub/ui/home/HomeScreenView.kt
+++ app/src/main/java/com/raylabs/laundryhub/ui/home/HomeScreenView.kt
@@ -33,6 +33,7 @@ import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import coil.compose.AsyncImage
 import com.raylabs.laundryhub.R
+import com.raylabs.laundryhub.ui.common.dummy.dummyState
 import com.raylabs.laundryhub.ui.common.util.SectionState
 import com.raylabs.laundryhub.ui.component.InfoCard
 import com.raylabs.laundryhub.ui.component.OrderStatusCard
@@ -206,49 +207,5 @@ fun CardList(state: List<TodayActivityItem>) {
 @Preview
 @Composable
 fun PreviewHomeScreen() {
-    val dummyState = HomeUiState(
-        user = SectionState(data = UserItem(displayName = "Jhon Doe")),
-        todayIncome = SectionState(
-            data = listOf(
-                TodayActivityItem(
-                    id = "1",
-                    name = "Customer A",
-                    totalPrice = "Rp 105.000,-",
-                    status = "lunas",
-                    statusColor = PurpleLaundryHub,
-                    packageDuration = "Express - 24H"
-                )
-            )
-        ),
-        summary = SectionState(data = DUMMY_SUMMARY_ITEM),
-        orderStatus = SectionState(
-            data = listOf(
-                PendingOrderItem(
-                    "1",
-                    "Ny Emy",
-                    "Express - 6H",
-                    "On Wash",
-                    "on WM #1",
-                    "17 Sep 25, 16.40 PM"
-                ),
-                PendingOrderItem(
-                    "2",
-                    "Gabriel",
-                    "Express - 24H",
-                    "On Wash",
-                    "on WM #2",
-                    "17 Sep 25, 16.40 PM"
-                ),
-                PendingOrderItem(
-                    "3",
-                    "Arifin",
-                    "Regular",
-                    "On Dry",
-                    "on DM #1",
-                    "17 Sep 25, 16.40 PM"
-                )
-            )
-        )
-    )
     HomeScreenContent(dummyState)
 }
