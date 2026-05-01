5f19fc596b00b0cb6185134869a61a9e16726016
Uray Febri <ufebri@live.com>
2026-05-01 22:47:54 +0700

    fix: add missing respondText import

--- backend/src/main/kotlin/com/raylabs/laundryhub/backend/plugins/Routing.kt
+++ backend/src/main/kotlin/com/raylabs/laundryhub/backend/plugins/Routing.kt
@@ -9,6 +9,7 @@ import io.ktor.http.HttpStatusCode
 import io.ktor.server.application.Application
 import io.ktor.server.application.call
 import io.ktor.server.response.respond
+import io.ktor.server.response.respondText
 import io.ktor.server.routing.get
 import io.ktor.server.routing.post
 import io.ktor.server.routing.routing
